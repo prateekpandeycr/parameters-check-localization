840b73ab60d4cdad59614ca3c7fe1dc5ee93f8f2
Prateek Pandey <prateekpandeycr@gmail.com>
2024-06-20 14:34:57 +0530

    angular json configuration for lang

--- projects/localizei18n/src/app/app.component.ts
+++ projects/localizei18n/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'localizei18n';
+  constructor(@Inject(LOCALE_ID) public localeId: string) {}
+  title = 'localization';
+  today: number = Date.now();
+  tasksCount = 3;
+  newTitle = $localize` Created by ${this.title}`;
+  localeList = [
+    { code: 'fr-FR', label: 'Franse' },
+    { code: 'es-ES', label: 'Espanish' },
+  ];
+
+  changeLanguage(code: string) {
+    this.localeId = code;
+  }
 }
